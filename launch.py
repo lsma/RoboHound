03921b4d9f5cd59af53a2db87138c088fddc3a8d
LSMA <stanasjes@gmail.com>
2017-09-19 23:32:57 +0000

    Formatting

--- launch.py
+++ launch.py
@@ -29,8 +29,10 @@ logging.info('Launching RoboHound')
 
 logger = logging.getLogger('discord')
 
-handler = logging.FileHandler(filename='robohound.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
+handler = logging.FileHandler( \
+    filename='robohound.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter( \
+    '%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
 logger.addHandler(handler)
 
 # Start the bot
